6c5d1a123b19aa41d887327169307c8225d5e11d
MatiasPineda <matias.pinedav@gmail.com>
2021-03-15 02:49:13 -0300

    problem 1

--- problems/problem1.py
+++ problems/problem1.py
@@ -1,4 +1,9 @@
-
+def multiples_3_and_5():
+    sum = 0
+    for i in range(1,1000):
+        if i % 3 == 0 or i % 5 == 0:
+            sum += i
+    return sum
 
 def solution():
-    print('totoot')
+    print(multiples_3_and_5())
