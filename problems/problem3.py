2b155f01f96898c07c536558c1f3a9b5c8883c4f
MatiasPineda <matias.pinedav@gmail.com>
2021-03-15 14:30:31 -0300

    Solved problem3.py

--- problems/problem3.py
+++ problems/problem3.py
@@ -1,5 +1,11 @@
-
-
+def largest_prime_factor(n):
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+    return n
 
 def solution():
-    pass
+    print(largest_prime_factor(600851475143))
