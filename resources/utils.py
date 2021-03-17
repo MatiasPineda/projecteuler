c4957225e8a11d4f70e6df4da7eebc035bc7182a
MatiasPineda <matias.pinedav@gmail.com>
2021-03-16 22:10:32 -0300

    Added prime_factors and prod functions to utils.py

---
+++ resources/utils.py
@@ -0,0 +1,28 @@
+from typing import Union
+
+
+def prime_factors(number: int) -> dict:
+    """
+    Takes an integer and returns every prime factor and their quantity
+    :param number: Integer
+    :return: dict of prime numbers as keys and their ammount as values
+    """
+    f = {}
+    i = 2
+    while number > 1 and number >= i:
+        if number % i == 0:
+            if i not in f:
+                f[i] = 1
+            else:
+                f[i] += 1
+            number //= i
+        else:
+            i += 1
+    return f
+
+
+def prod(num_list: Union[list, tuple, set]) -> int:
+    product = 1
+    for i in num_list:
+        product *= i
+    return product
